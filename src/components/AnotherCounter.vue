1c4e1122213ac9406648c8327b6e8e7573a0657d
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-10-06 19:04:49 +0300

    'Learning'

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment(100)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(150)">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import { mapMutations } from 'vuex';
+    import { mapActions } from 'vuex';
+    export default {
+        methods: {
+/*            ...mapMutations([
+                'increment',
+                'decrement'
+            ]),
+*/
+            ...mapActions([
+                'increment',
+                'decrement'
+            ]),
+            increment(by){
+                this.$store.dispatch('increment',by);
+            }
+        }
+    }
+</script>
